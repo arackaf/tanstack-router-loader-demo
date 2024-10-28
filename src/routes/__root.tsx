7846a00f8fdf10d2ea419831258d9b7cc4a41150
Adam Rackis <adamrackis@hotmail.com>
2024-10-27 20:27:18 -0500

    Add loading state

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,8 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet, useRouterState } from "@tanstack/react-router";
+
+const Loading = () => {
+  return <div className="fixed left-[50%] top-0 translate-x-[-50%] bg-yellow-300 rounded-b px-2 py-1">Loading ...</div>;
+};
 
 export const Route = createRootRoute({
   async beforeLoad({ location }) {
@@ -13,9 +17,13 @@ export const Route = createRootRoute({
 });
 
 function Root() {
+  const state = useRouterState();
+  const isNavigating = state.isLoading;
+
   return (
     <>
       <div className="p-x-2">
+        {isNavigating ? <Loading /> : null}
         <Outlet />
       </div>
     </>
