f785318fbc0ecd735dcf864e01faf90fcca42aad
Adam Rackis <adamrackis@hotmail.com>
2024-11-04 23:13:56 -0600

    work

--- src/routes/app/epics/index.tsx
+++ src/routes/app/epics/index.tsx
@@ -1,6 +1,7 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { epicsQueryOptions } from "../../../app/queries/epicsQuery";
+import { Fragment } from "react/jsx-runtime";
 
 export const Route = createFileRoute("/app/epics/")({
   loader({ context }) {
@@ -18,17 +19,17 @@ function Index() {
   return (
     <div className="p-3">
       <h3 className="text-red-500">Epics page!</h3>
-      <div className="flex flex-col gap-2 p-3">
+      <div className="inline-grid gap-x-8 gap-y-4 grid-cols-[auto_auto_auto] items-center p-3">
         {data.map((e, idx) => (
-          <div key={idx} className="flex gap-3">
+          <Fragment key={idx}>
             <div>{e.name}</div>
-            <Link to="/app/epics/$epicId" params={{ epicId: e.id }}>
+            <Link to="/app/epics/$epicId" params={{ epicId: e.id }} className="border p-1 rounded">
               View
             </Link>
-            <Link to="/app/epics/$epicId/edit" params={{ epicId: e.id }}>
+            <Link to="/app/epics/$epicId/edit" params={{ epicId: e.id }} className="border p-1 rounded">
               Edit
             </Link>
-          </div>
+          </Fragment>
         ))}
       </div>
     </div>
