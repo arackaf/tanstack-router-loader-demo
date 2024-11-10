f7422edd54297774edcac97def3d1b688e2fa922
Adam Rackis <adamrackis@hotmail.com>
2024-11-10 16:41:29 -0600

    revert removal of beforeLoad auth code

--- src/routes/app/route.tsx
+++ src/routes/app/route.tsx
@@ -2,17 +2,18 @@ import { createFileRoute, Link, Outlet, redirect, useRouter } from "@tanstack/re
 import { getCurrentUser } from "../../../backend/auth";
 
 export const Route = createFileRoute("/app")({
-  // async beforeLoad({}) {
-  //   const user = await getCurrentUser();
-  //   if (!user) {
-  //     throw redirect({
-  //       to: "/login",
-  //     });
-  //   }
-  //   document.cookie = `user=${user.id};path=/;max-age=31536000`;
+  async beforeLoad({}) {
+    const user = await getCurrentUser();
+    if (!user) {
+      throw redirect({
+        to: "/login",
+      });
+    }
+    document.cookie = `user=${user.id};path=/;max-age=31536000`;
 
-  //   return { user };
-  // },
+    return { user };
+  },
+
   gcTime: 1000 * 60 * 10,
   staleTime: 1000 * 60 * 10,
   component: () => {
@@ -21,7 +22,7 @@ export const Route = createFileRoute("/app")({
     return (
       <div>
         <div className="p-2 flex gap-4">
-          <span className="mr-7">Welcome: {"context.user.name"}</span>
+          <span className="mr-7">Welcome: {context.user.name}</span>
           <Link to="/app" activeOptions={{ exact: true }} className="[&.active]:font-bold">
             Home
           </Link>
