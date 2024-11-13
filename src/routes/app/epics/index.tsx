e5893dee6ba4571db5ea834efc95192eb1eceff1
Adam Rackis <adamrackis@hotmail.com>
2024-11-13 07:52:51 -0600

    work

--- src/routes/app/epics/index.tsx
+++ src/routes/app/epics/index.tsx
@@ -40,40 +40,42 @@ function Index() {
   const { data: epicsCount } = useSuspenseQuery(epicsCountQueryOptions(context.timestarted));
 
   return (
-    <div className="p-3">
+    <div className="flex flex-col gap-2 p-3">
       <h3 className="text-2xl">Epics page!</h3>
       <h3 className="text-lg">There are {epicsCount.count} epics</h3>
-      <div
-        className={`inline-grid gap-x-8 gap-y-4 grid-cols-[auto_auto_auto] items-center p-3 ${loading ? "opacity-40" : ""}`}
-      >
-        {epicsData.map((e, idx) => (
-          <Fragment key={idx}>
-            <div>{e.name}</div>
-            <Link to="/app/epics/$epicId" params={{ epicId: e.id }} className="border p-1 rounded">
-              View
+      <div>
+        <div
+          className={`inline-grid gap-x-8 gap-y-4 grid-cols-[auto_auto_auto] items-center p-3 ${loading ? "opacity-40" : ""}`}
+        >
+          {epicsData.map((e, idx) => (
+            <Fragment key={idx}>
+              <div>{e.name}</div>
+              <Link to="/app/epics/$epicId" params={{ epicId: e.id }} className="border p-1 rounded">
+                View
+              </Link>
+              <Link to="/app/epics/$epicId/edit" params={{ epicId: e.id }} className="border p-1 rounded">
+                Edit
+              </Link>
+            </Fragment>
+          ))}
+          <div className="flex gap-3">
+            <Link
+              to="/app/epics"
+              search={{ page: page - 1 }}
+              className="border p-1 rounded"
+              disabled={loading || page === 1}
+            >
+              Prev
             </Link>
-            <Link to="/app/epics/$epicId/edit" params={{ epicId: e.id }} className="border p-1 rounded">
-              Edit
+            <Link
+              to="/app/epics"
+              search={{ page: page + 1 }}
+              className="border p-1 rounded"
+              disabled={loading || !epicsData.length}
+            >
+              Next
             </Link>
-          </Fragment>
-        ))}
-        <div className="flex gap-3">
-          <Link
-            to="/app/epics"
-            search={{ page: page - 1 }}
-            className="border p-1 rounded"
-            disabled={loading || page === 1}
-          >
-            Prev
-          </Link>
-          <Link
-            to="/app/epics"
-            search={{ page: page + 1 }}
-            className="border p-1 rounded"
-            disabled={loading || !epicsData.length}
-          >
-            Next
-          </Link>
+          </div>
         </div>
       </div>
     </div>
