fe82c568d3dd3d5d993611540c3316dbd8db8ef7
Adam Rackis <adamrackis@hotmail.com>
2024-11-17 16:08:34 -0500

    add delay

--- backend/app.ts
+++ backend/app.ts
@@ -28,9 +28,11 @@ app.get("/api/tasks/overview", function (req, res) {
     INNER JOIN users u
     ON t.userId = u.id
     GROUP BY u.id
-  `).then(tasks => {
-    res.json(tasks);
-  });
+  `)
+    .then(tasks => new Promise(res => setTimeout(() => res(tasks), 750)))
+    .then(tasks => {
+      res.json(tasks);
+    });
 });
 
 app.get("/api/tasks", async function (req, res) {
